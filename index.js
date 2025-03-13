d460180ab993bb973a97c9f3f34ad2c17645b464
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-03-13 11:42:41 +0300

    lab completed

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ function writeCards(names) {
   return names.map(name =>`Thank you, ${name}, for the wonderful surprise gift!`)
 }
 
-// function countDown(names) {
-//     let number = 0
-//     while (number < names.length) {
-//         console.log(`Thank you, ${name[number]}, for the wonderful surprise gift!`)
-//         number--;
-//     }
-//     return names
-// }
-
 function countDown(number) {
   for (let count = number; count >= 0; count--) {
     console.log(count);
